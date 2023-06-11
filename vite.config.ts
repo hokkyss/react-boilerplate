afc792a0b1345f40bc84713201644fa2bdb7063d
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-06-11 10:52:27 +0700

    feat: take paths from tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { AliasOptions, defineConfig } from "vite";
+import tsConfig from "./tsconfig.json";
+
+const tsConfigPaths = tsConfig.compilerOptions.paths;
+const alias: AliasOptions = Object.entries(tsConfigPaths).map(
+  ([pathKey, [replacement]]) => ({
+    find: pathKey.replace("/*", ""),
+    replacement: path.resolve(__dirname, replacement.replace("/*", "")),
+  })
+);
 
 // https://vitejs.dev/config/
 export default defineConfig(async (_configEnv) => ({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "./src") }],
+    alias,
   },
 }));
