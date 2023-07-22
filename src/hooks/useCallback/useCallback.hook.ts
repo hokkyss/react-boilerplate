705b3851d40fe15faa998297a6a5685bd9857857
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-07-22 11:51:32 +0700

    feat: stable callback

--- src/hooks/useCallback/useCallback.hook.ts
+++ src/hooks/useCallback/useCallback.hook.ts
@@ -1,17 +1,27 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import {
   DependencyList,
   useMemo,
   useCallback as useReactCallback,
 } from "react";
 
-export type Callback<T extends Function> = T & { memoized: true };
+// Generalize all function types
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Func = (...args: any[]) => any;
 
-export default function useCallback<T extends Function>(
-  callback: T,
+type StableCallback<T extends Func> = T & { memoized: false };
+type MemoizedCallback<T extends Func> = T & { memoized: true };
+
+export type Callback<T extends Func> = StableCallback<T> | MemoizedCallback<T>;
+
+export function callback<T extends Func>(func: T): StableCallback<T> {
+  return Object.assign(func, { memoized: false } as const);
+}
+
+export default function useCallback<T extends Func>(
+  func: T,
   deps: DependencyList
-): Callback<T> {
-  const cachedCallback = useReactCallback(callback, deps);
+): MemoizedCallback<T> {
+  const cachedCallback = useReactCallback(func, deps);
 
   const memoedCallback = useMemo(
     () => Object.assign(cachedCallback, { memoized: true } as const),
